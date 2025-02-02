1c95cdf949bc41c645ef0111532e653b2f13fcfc
Igor Nascimento <igornalves@hotmail.com>
2025-02-02 12:15:55 -0300

    criando novas coisas para o projeto

--- src/main/java/com/maizenatv/assistenciawebsistema/models/Mensagens.java
+++ src/main/java/com/maizenatv/assistenciawebsistema/models/Mensagens.java
@@ -1,6 +1,11 @@
 package com.maizenatv.assistenciawebsistema.models;
 
+import java.util.UUID;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -9,4 +14,7 @@ import lombok.Data;
 @Table(name = "messages")
 public class Mensagens {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 }
