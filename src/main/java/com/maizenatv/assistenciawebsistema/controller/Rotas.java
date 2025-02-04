55512e6a861e8c335e705d3cf0c83a3ce6b6d567
guilherme Douglas <gui08@gmail.com>
2025-02-04 15:36:36 -0300

    alteracao basica para o projeto

--- src/main/java/com/maizenatv/assistenciawebsistema/controller/Rotas.java
+++ src/main/java/com/maizenatv/assistenciawebsistema/controller/Rotas.java
@@ -1,12 +1,14 @@
 package com.maizenatv.assistenciawebsistema.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.maizenatv.assistenciawebsistema.services.CustomerService;
 
+@Controller
 @RestController
 public class Rotas {
 
